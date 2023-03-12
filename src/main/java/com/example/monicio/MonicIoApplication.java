2c054dd31312dad679511798eec504f4bdd2daf8
HukoJlauII <polosatay_zebra@mail.ru>
2023-03-12 13:23:31 +0300

    Created server project with dependencies Added properties to application.properties

---
+++ src/main/java/com/example/monicio/MonicIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.monicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonicIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonicIoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/monicio/MonicIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonicIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
